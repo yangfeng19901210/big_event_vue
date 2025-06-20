433f7bdd881bc650a2d0e824434faf2adfc849d4
15525438831 <15525438831@163.com>
2025-06-20 16:09:35 +0800

    响应拦截器的定义以及代码优化

--- src/api/article.js
+++ src/api/article.js
@@ -1,27 +1,14 @@
-// 导入axios库
-import axios from 'axios'
-//定义变量记录公共前缀
-const baseURL = "http://localhost:8070";
-const instance = axios.create({baseURL});
+// // 导入axios库
+// import axios from 'axios'
+// //定义变量记录公共前缀
+// const baseURL = "http://localhost:8070";
+// const instance = axios.create({baseURL});
+import request from '@/util/request.js'
 // 获取所有文章信息函数
-export async function articleGetAllService(){
-    return await instance.get("/article/getAll")
-    .then(result=>{
-        // 保存数据
-        return result.data;
-    })
-    .catch(err=>{
-        log.info(err);
-    });
+export function articleGetAllService(){
+    return request.get("/article/getAll");
 }
 // 根据文章分类和发布状态搜索文章函数
-export async function articleSearchService(searchConditions){
-    return await instance.get("/article/search",{params:searchConditions})
-       .then(result=>{
-            // 保存数据
-            return result.data;
-       })
-       .catch(err=>{
-            log.info(err);
-       }); 
+export function articleSearchService(searchConditions){
+    return request.get("/article/search",{params:searchConditions}); 
 }
---
+++ src/util/request.js
@@ -0,0 +1,19 @@
+//定制请求的实例
+// 导入axios库
+import axios from 'axios'
+//定义变量记录公共前缀
+const baseURL = "http://localhost:8070";
+const instance = axios.create({baseURL});
+
+// 添加响应拦截器(本身就是异步处理)
+instance.interceptors.response.use(
+    result=>{
+        return result.data;
+    },
+    err=>{
+        alert('服务异常');
+        return Promise.reject(err);//异步的状态转化成失败的状态
+    }
+)
+// 导出实例
+export default instance;
