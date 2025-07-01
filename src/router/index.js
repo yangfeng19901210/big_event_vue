77e139d318080dfe2c35a142790f2effaf0f27e5
15525438831 <15525438831@163.com>
2025-07-01 14:16:24 +0800

    子路由应用

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,38 @@ import {createRouter,createWebHistory} from 'vue-router'
 // 导入组件
 import Loginvue from '@/views/Login.vue';
 import Layout from '@/views/Layout.vue';
+import ArticleCategory from '@/views/article/ArticleCategory.vue';
+import ArticleManage from '@/views/article/ArticleManage.vue';
+import UserInfo from '@/views/user/UserInfo.vue';
+import UserAvatar from '@/views/user/UserAvatar.vue';
+import UserResetPassword from '@/views/user/UserResetPassword.vue';
 // 定义路由关系
 const routes = [
   { path: '/login', component: Loginvue },
-  { path: '/', component: Layout }
+  { path: '/', component: Layout ,redirect: '/article/manage',
+    children: [
+      {
+        path: '/article/category',
+        component: ArticleCategory,
+      },
+      {
+        path: '/article/manage',
+        component: ArticleManage,
+      },
+      {
+        path: '/user/info',
+        component: UserInfo,
+      },
+      {
+        path: '/user/avatar',
+        component: UserAvatar,
+      },
+      {
+        path: '/user/resetPassword',
+        component: UserResetPassword,
+      }
+    ],
+  }
 ]
 // 创建路由器
 const router = createRouter({
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -19,13 +19,13 @@ import avatar from '@/assets/default.png'
             <div class="el-aside__logo"></div>
             <el-menu active-text-color="#ffd04b" background-color="#232323"  text-color="#fff"
                 router>
-                <el-menu-item index="category">
+                <el-menu-item index="/article/category">
                     <el-icon>
                         <Management />
                     </el-icon>
                     <span>文章分类</span>
                 </el-menu-item>
-                <el-menu-item index="article">
+                <el-menu-item index="/article/manage">
                     <el-icon>
                         <Promotion />
                     </el-icon>
@@ -38,19 +38,19 @@ import avatar from '@/assets/default.png'
                         </el-icon>
                         <span>个人中心</span>
                     </template>
-                    <el-menu-item index="basicInfo">
+                    <el-menu-item index="/user/info">
                         <el-icon>
                             <User />
                         </el-icon>
                         <span>基本资料</span>
                     </el-menu-item>
-                    <el-menu-item index="changeAvatar">
+                    <el-menu-item index="/user/avatar">
                         <el-icon>
                             <Crop />
                         </el-icon>
                         <span>更换头像</span>
                     </el-menu-item>
-                    <el-menu-item index="resetPassword">
+                    <el-menu-item index="/user/resetPassword">
                         <el-icon>
                             <EditPen />
                         </el-icon>
@@ -83,9 +83,10 @@ import avatar from '@/assets/default.png'
             </el-header>
             <!-- 中间区域 -->
             <el-main class="content">
-                <div style="width: 1470px; height: 660px;border: 1px solid red;">
+                <!-- <div style="width: 1470px; height: 660px;border: 1px solid red;">
                     内容展示区
-                </div>
+                </div> -->
+                <router-view></router-view>
             </el-main>
             <!-- 底部区域 -->
             <el-footer>大事件 ©2023 Created by 黑马程序员</el-footer>
