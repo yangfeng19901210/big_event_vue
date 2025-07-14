3539cbe954dd9527dbaf10f0c8f076349fb92f59
15525438831 <15525438831@163.com>
2025-07-14 14:51:02 +0800

    首页的昵称和用户头像设置

--- src/stores/userInfo.js
+++ src/stores/userInfo.js
@@ -2,17 +2,20 @@
 import {defineStore} from 'pinia';
 import {ref} from 'vue';
 
-export const userInfoStore = defineStore('userInfo',()=>{
-    //定义状态相关的内容
-    const info = ref({})
-
-    // 设置用户信息
+export const useUserInfoStore = defineStore('userInfo',()=>{
+    // 定义响应的内容
+    const info = ref({});
+    // 修改token的值
     const setInfo = (newInfo)=>{
         info.value = newInfo;
     }
-    // 删除用户信息
+    // 移除token的值
     const removeInfo = ()=>{
         info.value = {};
     }
-    return info,setInfo,removeInfo
-},{persist:true});
+    return {
+        info,setInfo,removeInfo
+    }
+},{
+    persist:true
+});
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -11,6 +11,21 @@ import {
     Setting
 } from '@element-plus/icons-vue'
 import avatar from '@/assets/default.png'
+
+import { userInfoService } from '@/api/user'
+import { useUserInfoStore } from '@/stores/userInfo.js'
+const userInfoStore = useUserInfoStore();
+
+// 调用函数获取用户信息
+const getUserInfo = async()=>{
+    // 获取用户信息
+    let result = await userInfoService();
+    console.log(result);
+    
+    // 数据存储到pinia中
+    userInfoStore.setInfo(result.data);
+}
+getUserInfo();
 </script>
 
 <template>
@@ -68,10 +83,10 @@ import avatar from '@/assets/default.png'
         <el-container>
             <!-- 头部区域 -->
             <el-header>
-                <div>黑马程序员：<strong>东哥</strong></div>
+                <div>黑马程序员：<strong>{{userInfoStore.info.nickname}}</strong></div>
                 <el-dropdown placement="bottom-end">
                     <span class="el-dropdown__box">
-                        <el-avatar :src="avatar" />
+                        <el-avatar :src="userInfoStore.info.userPic?userInfoStore.info.userPic:avatar" />
                         <el-icon>
                             <CaretBottom />
                         </el-icon>
